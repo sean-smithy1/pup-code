8ad8745b8e75a48a237fae9cbe74e37606c6302c
Sean Smith <sean.smithy1@gmail.com>
2018-07-27 16:20:22 +1000

    Updated IP

--- environments/production/manifests/site.pp
+++ environments/production/manifests/site.pp
@@ -8,7 +8,7 @@ node default {
 node '1804-dnsmasq' {
 
   netplan::interface { 'ens3':
-    addresses => [ '192.168.0.20/24' ],
+    addresses => [ '192.168.0.4/24' ],
     dns => [ '192.168.0.4', '8.8.8.8' ],
     search => [ 'comapps.net' ],
     gateway4 => '192.168.0.15',
